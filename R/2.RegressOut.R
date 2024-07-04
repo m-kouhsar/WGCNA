68ea42cc9045acb4a029ed2e38fa6e2ccfab1648
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-07-04 16:42:06 +0100

    Add files via upload

--- R/2.RegressOut.R
+++ R/2.RegressOut.R
@@ -2,18 +2,16 @@ args <- commandArgs(TRUE)
 
 input_expression <- args[1]
 input_phenotype <- args[2]
-trait <- args[3]
-variables_fact <- args[4]
-variables_num <- args[5]
-model_lm <- args[6]
-out_pref <- args[7]
+variables_fact <- args[3]
+variables_num <- args[4]
+model_lm <- args[5]
+out_pref <- args[6]
 
 sink(paste0(out_pref,".RegressOut.log.txt"))
 
 cat("Input arguments:\n")
 cat("    Input expression file: ",input_expression,"\n")
 cat("    Input phenotype file: ",input_phenotype,"\n")
-cat("    Trait variable: ",trait,"\n")
 cat("    Factor variables: ",variables_fact,"\n")
 cat("    Numeric variables: ",variables_num,"\n")
 cat("    Regression model: ",model_lm,"\n")
@@ -38,19 +36,17 @@ cat("\n")
 variables_num <- str_split(variables_num,pattern = ',',simplify = T)[1,]
 variables_fact <- str_split(variables_fact,pattern = ',',simplify = T)[1,]
 
-pheno2 <- as.numeric(as.factor(pheno[,trait]))
 for (i in 1:length(variables_fact)) {
-  pheno2 <- cbind.data.frame(pheno2,as.numeric(as.factor(pheno[,variables_fact[i]])))
+  pheno[,variables_fact[i]] <- as.numeric(as.factor(pheno[,variables_fact[i]]))
 }
 for (i in 1:length(variables_num)) {
-  pheno2 <- cbind.data.frame(pheno2,as.numeric(pheno[,variables_num[i]]))
+  pheno[,variables_num[i]] <- as.numeric(pheno[,variables_num[i]])
 }
-names(pheno2) <- c(trait,variables_fact,variables_num)
 
 ##############################################################
 
 cat("Regressing out covariates using linear regression...\n")
-fit.lm <- function(x,model=model_lm,data=pheno2){
+fit.lm <- function(x,model=model_lm,data=pheno){
   x <- as.numeric(x)
   lm_vars <- all.vars(as.formula(model))
   for(j in 1:length(lm_vars)){
@@ -69,7 +65,7 @@ fit.lm <- function(x,model=model_lm,data=pheno2){
 nThreads <- max(1, parallel::detectCores(), na.rm = TRUE)-1
 
 cl<- makeCluster(nThreads)
-expr.norm<-t(parApply(cl, expr , 1, fit.lm, model_lm,pheno2))
+expr.norm<-t(parApply(cl, expr , 1, fit.lm, model_lm,pheno))
 
 stopCluster(cl) 
 
