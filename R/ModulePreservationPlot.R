f3fc79f751baf759d890334057d39343f2a5300f
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-09-23 00:41:08 +0100

    Update ModulePreservationPlot.R

--- R/ModulePreservationPlot.R
+++ R/ModulePreservationPlot.R
@@ -38,27 +38,3 @@ module.preservation.plot <- function(MP.Data , Modules,title.medrank="",title.zs
 }
 
 #####################################################################################################################################
-
-#setwd("C:/Users/mk693/OneDrive - University of Exeter/Desktop/2021/NIH/Brain EWAS Paper")
-#library(stringr)
-
-#ModulePres_file="WGCNA/mad.0.5.lm.v2.pow3/Pitts.BDR.All.Braak.max.1000.gold.1000_ModulePreservation.Rdata"
-#module.names <- "seashell4,mediumpurple3,thistle2,green4,firebrick3,sienna2,lightcoral,antiquewhite,magenta1,lightcyan,burlywood,linen,thistle3,lavenderblush,darkgoldenrod1,tan2,greenyellow,whitesmoke,orange,midnightblue,mistyrose,darkred,lightblue1"
-#out_pref = "Figures/wgcna.mad.0.5.v2.pow3/Module.Preservation/Pitts.All.BDR.All3"
-
-#load(ModulePres_file)
-#module.names = str_split(module.names,pattern = ',',simplify = T)[1,]
-
-#result <- module.preservation.plot(MP.Data = mp , Modules = module.names,title.medrank = "Median rank for module preservation of PITT-ADRC in BDR",
-#                                   title.zsummary = "Z summary for module preservation of PITT-ADRC in BDR")
-
-#tiff(file=paste0(out_pref,".ModulePreservation.MedianRank.tiff"), wi=10, h=8,units = "in",res = 500) 
-#result$Plot.Med.Rank
-#graphics.off()
-
-
-#tiff(file=paste0(out_pref,".ModulePreservation.ZSummary.tiff"), wi=10, h=8,units = "in",res = 500) 
-#result$Plot.Z.Summary
-#graphics.off()
-
-
