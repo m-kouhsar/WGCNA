39068d0483dbcbe70c139f103930f846e8d0b52d
Morteza Kouhsar <59332098+m-kouhsar@users.noreply.github.com>
2024-12-16 15:06:21 +0000

    Update mahalanobis.outlier.R

--- R/mahalanobis.outlier.R
+++ R/mahalanobis.outlier.R
@@ -23,6 +23,7 @@ mahalanobis.outlier <- function(Data , method = "pca", plot.title=NA , seed = NA
   center_ <- colMeans(Data.2D)
   cov_ <- cov(Data.2D)
   
+  # Calculating the squared Mahalanobis distance
   Data.2D$mdist <- mahalanobis(
     x = Data.2D,
     center = center_,
@@ -42,29 +43,45 @@ mahalanobis.outlier <- function(Data , method = "pca", plot.title=NA , seed = NA
   ellipse_ <- as.data.frame(ellipse_)
   colnames(ellipse_) <- colnames(Data.2D)[1:2]
   
-  Data.2D$p <- pchisq(Data.2D$mdist, df = 2, lower.tail = FALSE)
+  Data.2D$pchisq <- pchisq(Data.2D$mdist, df = 2, lower.tail = FALSE)
   
   Data.2D <- Data.2D %>%
     mutate(Outlier = ifelse(mdist > cutoff, 'Yes', 'No'))
   if(method == "pca"){
-    p <- ggplot(Data.2D, aes(x = PC1 , y = PC2, color = Outlier))
+    p1 <- ggplot(Data.2D, aes(x = PC1 , y = PC2, color = Outlier))+
+      xlab(paste0("PC1 ",round(pc.importance[1],digits = 2)*100,"%"))+
+      ylab(paste0("PC2 ",round(pc.importance[2],digits = 2)*100,"%"))
     plot.subtitle = "Dimentinality reduction method: PCA"
   }
   if(method == "tsne"){
-    p <- ggplot(Data.2D, aes(x = D1 , y = D2, color = Outlier))
+    p1 <- ggplot(Data.2D, aes(x = D1 , y = D2, color = Outlier))
     plot.subtitle = "Dimentinality reduction method: tSNE"
   }
   if(is.na(plot.title)){
-    plot.title = "Outlier detection using Mahalanobis distance and Chi-Squared Distribution"
+    plot.title = ""
   }
-  p <- p +
+  p1 <- p1 +
     geom_point(size = 3) +
     geom_point(aes(center_[1], center_[2]) , size = 5 , color = 'blue') +
     geom_polygon(data = ellipse_, fill = 'white', color = 'black', alpha = 0.3) +
     scale_color_manual(values = c('gray44', 'red')) +
-    labs(title = plot.title,subtitle = plot.subtitle) +
+    labs(title =plot.title,subtitle = paste0("Outliers in 2D Plot, ",plot.subtitle)) +
     theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5))
+    theme(plot.title = element_text(hjust = 0))
   
-  return(list(Data.2D = Data.2D , Plot=p))
+  p2 <- ggplot() + geom_point(data = Data.2D , aes(x=mdist , y= pchisq , color = Outlier)) + theme_bw() +
+    scale_color_manual(values = c('black', 'red'))+
+    geom_vline(xintercept = cutoff , color="red")+
+    ylab("Chi-Square probability")+
+    xlab("Square Mahalanobis distance")+
+    labs(title =plot.title,subtitle = paste0("Outliers in Chi-Square Plot, ",plot.subtitle))
+  
+  Data.2D$qchisq =qchisq(ppoints(length(Data.2D$mdist)), df = 2)
+  p3 <- ggplot() + geom_point(data = Data.2D, aes(x=sort(qchisq), y=sort(mdist))) + theme_bw() +
+    geom_abline(aes(slope = 1, intercept = 0),color="red")+
+    xlab("Chi-Square quantiles")+
+    ylab("Square Mahalanobis distance quantiles")+
+    labs(title =plot.title,subtitle = paste0("QQ Plot, ",plot.subtitle))
+  
+  return(list(Data.2D = Data.2D , Plot.2D=p1 , Plot.Dist=p2 , Plot.QQ = p3))
 }
